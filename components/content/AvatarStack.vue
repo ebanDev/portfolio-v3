4a889c153a716471c88c3625bfba1ca8669285fe
Eban <contact@eban.eu.org>
2025-05-21 17:57:14 +0200

    Add format attribute to NuxtImg components in AvatarStack for optimized image rendering

--- components/content/AvatarStack.vue
+++ components/content/AvatarStack.vue
@@ -11,21 +11,21 @@ function handleClick(): void {
 <template>
   <div class="group flex items-center mb-6 w-26 h-20 relative cursor-pointer" @click="handleClick">
     <div class="size-20 left-1 rounded-full absolute shadow-md z-20">
-      <NuxtImg :src="avatars[0]" alt="Avatar" class="w-full h-full rounded-full" height="128" width="128" />
+      <NuxtImg :src="avatars[0]" alt="Avatar" class="w-full h-full rounded-full" height="128" width="128" format="avif" />
     </div>
     <div :class="[
       'rounded-full shadow-md absolute bottom-0 z-10 transition-all duration-150 ease-in-out',
       'size-18 left-5 group-hover:size-20 group-hover:left-16',
       isActive && 'size-20 left-16'
     ]">
-      <NuxtImg :src="avatars[1]" alt="Avatar" class="w-full h-full rounded-full" height="128" width="128" />
+      <NuxtImg :src="avatars[1]" alt="Avatar" class="w-full h-full rounded-full" height="128" width="128" format="avif" />
     </div>
     <div :class="[
       'rounded-full shadow-md absolute bottom-0 z-0 transition-all duration-150 ease-in-out',
       'size-16 left-9 group-hover:size-20 group-hover:left-32',
       isActive && 'size-20 left-32'
     ]">
-      <NuxtImg :src="avatars[2]" alt="Avatar" class="w-full h-full rounded-full" height="128" width="128" />
+      <NuxtImg :src="avatars[2]" alt="Avatar" class="w-full h-full rounded-full" height="128" width="128" format="avif" />
     </div>
   </div>
 </template>
